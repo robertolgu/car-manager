52d5b271a6dbd44a4b1bdf551cafc4890e92288e
Roberto Lopez <r.lopezgunti@gmail.com>
2022-03-20 19:33:04 +0100

    CrossOrigin CarController

--- src/main/java/com/vexiza/carmanager/controller/car/CarController.java
+++ src/main/java/com/vexiza/carmanager/controller/car/CarController.java
@@ -4,12 +4,14 @@ package com.vexiza.carmanager.controller.car;
 import com.vexiza.carmanager.repository.car.CarRepository;
 import com.vexiza.carmanager.utils.DataUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/")
 public class CarController {
